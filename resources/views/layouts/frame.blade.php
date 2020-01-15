662a44d5471bc4618eaaa627bdac766ca100166c
Johnvique <johnvique@gmail.com>
2020-01-15 16:17:23 +0300

    title_change

--- resources/views/layouts/frame.blade.php
+++ resources/views/layouts/frame.blade.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Competence Based Curriculum</title>
+  <title>School Admin Panel</title>
 
   <!-- Custom fonts for this template-->
 <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
